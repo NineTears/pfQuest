6bc675db3d79a89cd2c39f4c94615009083ed43c
NineTears <hellowind@live.com>
2024-03-04 21:58:48 +0800

    更新 customfunctions.lua

--- customfunctions.lua
+++ customfunctions.lua
@@ -1,6 +1,29 @@
 
+local function strmatch(str, pattern, index)
+	if type(str) ~= "string" and type(str) ~= "number" then
+		error(format("bad argument #1 to 'match' (string expected, got %s)", str and type(str) or "no value"), 2)
+	elseif type(pattern) ~= "string" and type(pattern) ~= "number" then
+		error(format("bad argument #2 to 'match' (string expected, got %s)", pattern and type(pattern) or "no value"), 2)
+	elseif index and type(index) ~= "number" and (type(index) ~= "string" or index == "") then
+		error(format("bad argument #3 to 'match' (number expected, got %s)", index and type(index) or "no value"), 2)
+	end
+
+	local i1, i2, match, match2 = string.find(str, pattern, index)
+
+	if not match and i2 and i2 >= i1 then
+		return string.sub(str, i1, i2)
+	elseif match2 then
+		local matches = {string.find(str, pattern, index)}
+		tremove(matches, 2)
+		tremove(matches, 1)
+		return unpack(matches)
+	end
+
+	return match
+end
+
 function containsChinese(inputString)
-    return string.match(inputString, "[\128-\255]") ~= nil
+    return strmatch(inputString, "[\128-\255]") ~= nil
 end
 
 function cnenQuestTitle(title, mode)
